e0296fb73d405912cded58be6dce5ed58a84a1d0
Keven Mingoranze <keven.ogi@hotmail.com>
2023-06-12 17:06:57 -0300

    refazendo area habilidades

---
+++ assets/js/skill.js
@@ -0,0 +1,8 @@
+let skillJason = [
+    {id:1, name:'HTML', logo:'./images/html.png', certificado:'./images/certificados/chtml.jpg'},
+    {id:2, name:'CSS', logo:'./images/css.png', certificado:'./images/certificados/ccss.jpg'},
+    {id:3, name:'Javascript', logo:'./images/js.png', certificado:'./images/certificados/cjs.jpg'},
+    {id:4, name:'Typescript', logo:'./images/ts.png', certificado:'./images/certificados/cts.jpg'},
+    {id:5, name:'React Native', logo:'./images/react.png', certificado:'./images/certificados/creact.jpg'},
+    {id:6, name:'Tailwind', logo:'./images/tailwind.png', certificado:'./images/certificados/ctailwind.jpg'},
+];
