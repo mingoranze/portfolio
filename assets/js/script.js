ec706562a86af6eb01f9aee5f8f7293ce9bb63d2
Keven Mingoranze <keven.hm@outlook.com>
2023-06-12 19:14:59 -0300

    fazendo push para test mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 skillJason.map((item)=>{
-    let skillItem = document.querySelector('')
+    let skillItem = document.querySelector('#skill-item').cloneNode(true);
+
+    skillItem.querySelector('p').innerHTML = item.name;
+    skillItem.querySelector('img').src = item.logo;
+
+    document.querySelector('#skill-area').append(skillItem);
 });
 
