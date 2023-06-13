6d4e61b761be2d6d5f943dc6ad6e4742abeaca09
Keven Mingoranze <keven.hm@outlook.com>
2023-06-12 21:06:54 -0300

    finalizado a section habilidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
-skillJason.map((item)=>{
+skillJason.map((item, index)=>{
     let skillItem = document.querySelector('#skill-item').cloneNode(true);
 
+    skillItem.setAttribute('data-key', index);
     skillItem.querySelector('p').innerHTML = item.name;
     skillItem.querySelector('img').src = item.logo;
+    skillItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        let key = e.target.closest('#skill-item').getAttribute('data-key');
+        document.querySelector('#imgModal').src = skillJason[key].certificado;
+
+        document.querySelector('#modal').style.opacity = 0;
+        document.querySelector('#modal').style.display = 'flex';
+        setTimeout(()=>{
+            document.querySelector('#modal').style.opacity = 1;
+        },200);
+    })
 
     document.querySelector('#skill-area').append(skillItem);
 });
 
+function closeModal(){
+    document.querySelector('#modal').style.opacity = 0;
+    setTimeout(()=>{    
+        document.querySelector('#modal').style.display = 'none';
+    },200);
+}
--- assets/js/skill.js
+++ assets/js/skill.js
@@ -1,8 +1,8 @@
 let skillJason = [
-    {id:1, name:'HTML', logo:'../assets/images/html.png', certificado:'./images/certificados/chtml.jpg'},
-    {id:2, name:'CSS', logo:'../assets/images/css.png', certificado:'./images/certificados/ccss.jpg'},
-    {id:3, name:'Javascript', logo:'../assets//images/js.png', certificado:'./images/certificados/cjs.jpg'},
-    {id:4, name:'Typescript', logo:'../assets//images/ts.png', certificado:'./images/certificados/cts.jpg'},
-    {id:5, name:'React Native', logo:'../assets//images/react.png', certificado:'./images/certificados/creact.jpg'},
-    {id:6, name:'Tailwind', logo:'../assets//images/tailwind.png', certificado:'./images/certificados/ctailwind.jpg'},
+    {id:1, name:'HTML', logo:'../assets/images/html.png', certificado:'../assets/images/certificados/chtml.jpg'},
+    {id:2, name:'CSS', logo:'../assets/images/css.png', certificado:'../assets/images/certificados/chtml.jpg'},
+    {id:3, name:'Javascript', logo:'../assets//images/js.png', certificado:'../assets/images/certificados/cjs.jpg'},
+    {id:4, name:'Typescript', logo:'../assets//images/ts.png', certificado:'../assets/images/certificados/cts.jpg'},
+    {id:5, name:'React Native', logo:'../assets//images/react.png', certificado:'../assets/images/certificados/creact.jpg'},
+    {id:6, name:'Tailwind', logo:'../assets//images/tailwind.png', certificado:'../assets/images/certificados/ctailwind.jpg'},
 ];
