11a73f8973984643ab5a9e0b100094cd2a47b647
Keven Mingoranze <keven.ogi@hotmail.com>
2023-07-07 16:08:06 -0300

    aparentemente finalizado

--- assets/js/skill.js
+++ assets/js/skill.js
@@ -3,6 +3,6 @@ let skillJason = [
     {id:2, name:'CSS', logo:'../assets/images/css.png', certificado:'../assets/images/certificados/chtml.jpg'},
     {id:3, name:'Javascript', logo:'../assets//images/js.png', certificado:'../assets/images/certificados/cjs.jpg'},
     {id:4, name:'Typescript', logo:'../assets//images/ts.png', certificado:'../assets/images/certificados/cts.jpg'},
-    {id:5, name:'React Native', logo:'../assets//images/react.png', certificado:'../assets/images/certificados/creact.jpg'},
+    {id:5, name:'React Native', logo:'../assets//images/react.png', certificado:'../assets/images/certificados/crn.jpg'},
     {id:6, name:'Tailwind', logo:'../assets//images/tailwind.png', certificado:'../assets/images/certificados/ctailwind.jpg'},
 ];
